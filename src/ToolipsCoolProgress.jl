a1f7ca8ee260f4fa44d07ce81a1179ef65fc531c
emmy <52672675+emmaccode@users.noreply.github.com>
2024-01-02 03:45:43 -0700

    Add files via upload

--- src/ToolipsCoolProgress.jl
+++ src/ToolipsCoolProgress.jl
@@ -1,22 +1,64 @@
 module ToolipsCoolProgress
 using Toolips
 using ToolipsSVG
+using ToolipsSession: AbstractComponentModifier
 
 function circular_progress(name::String, args::Pair{String, <:Any} ...; keyargs ...)
-    progrwindow = Component("progressbar", name, width = 100px, height = 100px)
-    progrwindow.tag = "svg"
+    progrwindow = Component(name, "cprogress")
+    progrwindow.tag, progrwindow[:width], progrwindow[:height] = "svg", 100px, 100px
     indicator = ToolipsSVG.circle("$name-indicator", cx = 50px, cy = 50px, r = 40px)
     track = ToolipsSVG.circle("$name-track", cx = 50px, cy = 50px, r = 40px)
+    style!(indicator, "fill" => "transparent", "stroke-width" => 7px, "stroke" => "darkgray", 
+    "transition" => 1seconds)
     style!(track, "fill" => "transparent", "stroke-width" => 10px, "stroke" => "lightblue", 
-    "stroke-dasharray" => 20px, "dashoffset" => 20px)
+    "stroke-dasharray" => 251px, "stroke-dashoffset" => 20px, "transition" => 1seconds)
     size = 100
-    strokeWidth = 10
+    strokew = 10
     center = size / 2
-    radius = center - strokeWidth 
-    progress = 0
-    arcLength = 2 * π * radius
-    arcOffset = arcLength * ((100 - progress)/100) 
-    progrwindow::Component{:progressbar}
+    radius = center - strokew 
+    progress = 25
+    arclen = 2 * π * radius
+    arc = Int64(round(arclen * ((100 - progress)/100))) 
+    push!(progrwindow, indicator, track)
+    style!(track, "stroke-dashoffset" => "$(arc)px")
+    progrwindow[:arclen] = arclen
+    progrwindow::Component{:cprogress}
 end
+
+function circular_progress_text(name::String, args::Pair{String, <:Any} ...; keyargs ...)
+    progrwindow = Component(name, "cprogress")
+    progrwindow.tag, progrwindow[:width], progrwindow[:height] = "svg", 100px, 100px
+    indicator = ToolipsSVG.circle("$name-indicator", cx = 50px, cy = 50px, r = 40px)
+    track = ToolipsSVG.circle("$name-track", cx = 50px, cy = 50px, r = 40px)
+    style!(indicator, "fill" => "transparent", "stroke-width" => 7px, "stroke" => "darkgray", 
+    "transition" => 1seconds)
+    style!(track, "fill" => "transparent", "stroke-width" => 10px, "stroke" => "lightblue", 
+    "stroke-dasharray" => 251px, "stroke-dashoffset" => 20px, "transition" => 1seconds)
+    size = 100
+    strokew = 10
+    center = size / 2
+    radius = center - strokew 
+    progress = 25
+    arclen = 2 * π * radius
+    arc = Int64(round(arclen * ((100 - progress)/100))) 
+    push!(progrwindow, indicator, track)
+    style!(track, "stroke-dashoffset" => "$(arc)px")
+    progrwindow[:arclen] = arclen
+    progrwindow::Component{:cprogress}
+end
+
+function update_progress!(cm::AbstractComponentModifier, comp::Component{:cprogress}, n::Int64)
+    arclen = comp[:arclen]
+    arc = Int64(round(arclen* ((100 - n)/100)))
+    style!(cm, comp[:children]["$(comp.name)-track"], "stroke-dashoffset" => "$(arc)px")
+    return
+end
+
+
+function update_progress!(cm::AbstractComponentModifier, comp::Component{:textcprogress}, n::Int64, text::String)
+
+end
+
+export update_progress!
 
 end # module ToolipsCoolProgress
